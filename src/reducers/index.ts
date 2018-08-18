a627c45a1e61c2eeed62246a246c291366522d2d
kouki <kouworld0123@gmail.com>
2018-08-18 16:15:20 +0900

    reducerをまとめた

--- src/reducers/index.ts
+++ src/reducers/index.ts
@@ -1,6 +1,14 @@
 import { combineReducers } from 'redux';
+import BoardState from './Board/BoardState';
+import CommentState from './Comment/CommentState';
+import TokenState from './Token/TokenState';
+import userState from './User/UserState';
 
 const rootReducer = combineReducers({
+  boardState: BoardState,
+  commentState: CommentState,
+  tokenState: TokenState,
+  userState: userState
 });
 
 export default rootReducer;
