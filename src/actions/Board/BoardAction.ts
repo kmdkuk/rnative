d48bf496125a0368e09b69d89eb18a10d0b99727
kouki <kouworld0123@gmail.com>
2018-08-18 15:01:08 +0900

    make board actions

---
+++ src/actions/Board/BoardAction.ts
@@ -0,0 +1,21 @@
+import { BoardActionType } from "./BoardActionType";
+
+export interface ISelectBoard {
+  type: BoardActionType.SELECT_BOARD
+  id: number
+}
+
+export interface ICommentsRead {
+  type: BoardActionType.COMMENTS_READ
+  comments: string
+}
+
+export interface ITokenChange {
+  type: BoardActionType.TOKEN_CHANGE
+  token: string
+}
+
+export interface ISelectUser {
+  type: BoardActionType.SELECT_USER
+  id: number
+}
---
+++ src/actions/Board/BoardActionType.ts
@@ -0,0 +1,8 @@
+export enum BoardActionType {
+  SELECT_BOARD = "SELECT_BOARD",
+  COMMENTS_READ = "COMMENTS_READ",
+  TOKEN_CHANGE = "TOKEN_CHANGE",
+  SELECT_USER = "SELECT_USER"
+}
+
+export default BoardActionType;
