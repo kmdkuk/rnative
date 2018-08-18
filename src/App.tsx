662b7dc178b1988a2c80fb65d1b8b737e4d591ec
kouki <kouworld0123@gmail.com>
2018-08-18 16:12:05 +0900

    アプリケーションの調整

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,15 @@
 import * as React from 'react';
 import { Component } from 'react';
-import { Platform, StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import { Provider } from "react-redux";
+import { createStore } from "redux";
+import rootReducer from './reducers';
+import BoardList from './containers/BoardList/BoardList';
+import CommentAdd from './containers/CommentAdd/CommentAdd';
+import CommentList from './containers/CommentList/CommentList';
+
+
+let store = createStore(rootReducer);
 
 interface Props { };
 export default class App extends Component<Props> {
@@ -11,11 +20,16 @@ export default class App extends Component<Props> {
   }
   render() {
     return (
-      <View style={styles.container}>
-        <Text style={styles.welcome}>
-          React Native! and Rails5.0!!
+      <Provider store={store}>
+        <View style={styles.container}>
+          <Text style={styles.welcome}>
+            React Native! and Rails5.0!!
         </Text>
-      </View>
+          <BoardList />
+          <CommentAdd />
+          <CommentList />
+        </View>
+      </Provider>
     );
   }
 }
